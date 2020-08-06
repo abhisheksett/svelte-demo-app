5e484a7fe759e635221cf73cb8aa5a3c8f1376ca
abhisheksett <aabhishek.sett@gmail.com>
2020-08-06 18:16:05 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# This is a sample Svelte app
+
+This app is created along with Frontend Masters Svelte course
---
+++ src/App.svelte
@@ -0,0 +1,70 @@
+<script>
+
+	import {onMount} from 'svelte';
+	import {select} from './select';
+	import Welcome from './screens/Welcome.svelte';
+
+	let state = 'welcome'; //or 'playing'
+	let celebs_promise;
+	let selection;
+
+	const start = async (e) => {
+		const {celebs, lookup} = await celebs_promise;
+		selection = select(celebs, lookup, e.detail.category.slug);
+		state = 'playing';
+	};
+
+	const load_celebs = async () => {
+		const res = await fetch('https://cameo-explorer.netlify.app/celebs.json');
+		const data = await res.json();
+
+		console.log(data);
+
+		const lookup = new Map();
+		data.forEach(c => {
+			lookup.set(c.id, c)
+		});
+
+		const subset = new Set();
+		data.forEach(c => {
+			if(c.reviews > 50) {
+				subset.add(c);
+				c.similar.forEach(id => {
+					subset.add(lookup.get(id));
+				})
+			}
+		});
+
+		return {
+			celebs: Array.from(subset),
+			lookup
+		}
+	};
+
+	onMount(() => {
+		celebs_promise = load_celebs();
+	});
+
+</script>
+<main>
+	{#if state === 'welcome'}
+		<Welcome on:select={start}/>
+	{:else if state === 'playing'}
+		<p>Playing screen</p>
+	{/if}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 800px;
+		margin: 0 auto;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
+
+	
+</style>
